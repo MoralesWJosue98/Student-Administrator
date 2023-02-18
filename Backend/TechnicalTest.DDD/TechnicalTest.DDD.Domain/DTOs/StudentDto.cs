4ac05fcb84422c7f0558f9c1b5cdb02c47e490d6
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-18 17:54:03 -0600

    Add: Student DTOs :necktie:

---
+++ Backend/TechnicalTest.DDD/TechnicalTest.DDD.Domain/DTOs/StudentDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalTest.DDD.Domain.DTOs
+{
+    public class StudentDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+        public string LastName { get; set; }
+        public string Career { get; set; }
+        public string AdmissionDate { get; set; }   
+
+
+    }
+}
