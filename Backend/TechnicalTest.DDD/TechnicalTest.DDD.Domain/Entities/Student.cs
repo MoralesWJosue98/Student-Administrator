5bc780fba656a8f063988a33463588475fc09ea2
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-18 14:55:53 -0600

    Add: Student Entity and Student Value Objects structure :construction:

---
+++ Backend/TechnicalTest.DDD/TechnicalTest.DDD.Domain/Entities/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalTest.DDD.Domain.Entities
+{
+    internal class Student
+    {
+        // TODO: change to value objects respective fields
+        // TODO: Value Objects Implementation
+        public int ID { get; set; }
+
+        public string Name { get; set; }    
+
+        public string LastName { get; set; }    
+
+        public string Career { get; set; }
+
+        public DateTime AdmissionDate { get; set; } 
+
+
+
+
+
+    }
+}
