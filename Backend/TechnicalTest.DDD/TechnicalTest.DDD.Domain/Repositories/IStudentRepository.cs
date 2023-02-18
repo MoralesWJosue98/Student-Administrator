2a3516e9b7f711b4345dd4b85a80eb1f15cb013b
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-18 15:50:31 -0600

    Add: Student Repository Comments :bulb:

--- Backend/TechnicalTest.DDD/TechnicalTest.DDD.Domain/Repositories/IStudentRepository.cs
+++ Backend/TechnicalTest.DDD/TechnicalTest.DDD.Domain/Repositories/IStudentRepository.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TechnicalTest.DDD.Domain.Repositories
 {
-    internal interface IStudentRepository
+    public interface IStudentRepository<TEntity> where TEntity : class
     {
+        // Establishing the operations that will be needed
+        Task AddAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);   
+        Task DeleteAsync(TEntity entity);
+        Task<TEntity> GetByIdAsync(int id, IEnumerable<string> entitiesToInclude = null);
+        Task<IEnumerable<TEntity>> GetAllAsync(int skip, int limit, IEnumerable<Expression<Func<TEntity,bool>>> predicates = null, IEnumerable<string> entitiesToInclude = null);
+
     }
 }
