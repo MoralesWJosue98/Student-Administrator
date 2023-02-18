73586a6933dd2185970f03369cc220b454c427eb
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-18 15:53:28 -0600

    Add: Common Pagination Params :triangular_flag_on_post:

---
+++ Backend/TechnicalTest.DDD/TechnicalTest.DDD.Domain/Common/Pagination.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalTest.DDD.Domain.Common
+{
+    public class Pagination
+    {
+        public int skip { get; set; } = 0;
+        public int limit { get; set; } = 10;
+    }
+}
