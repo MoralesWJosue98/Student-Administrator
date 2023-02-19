e389896a57acaef76084e3c5769cc73e5ecc0033
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-18 18:07:13 -0600

    Add: Student Mappers :card_file_box:

---
+++ Backend/TechnicalTest.DDD/TechnicalTest.DDD.Infrastructure/Mappers/MappingStudent.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicalTest.DDD.Domain.DTOs;
+using TechnicalTest.DDD.Domain.Entities;
+
+namespace TechnicalTest.DDD.Infrastructure.Mappers
+{
+    public class MappingStudent : Profile
+    {
+        public MappingStudent()
+        {
+            CreateMap<StudentDto, Student>().ReverseMap();
+        }
+    }
+}
