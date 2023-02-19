256056d91ba25eb819b8e9e3cd38dd6ab7879e0a
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-18 18:19:00 -0600

    Add: PermissionEntityConfig :card_file_box:

---
+++ Backend/TechnicalTest.DDD/TechnicalTest.DDD.Infrastructure/Database/PermissionEnitityStudentConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicalTest.DDD.Domain.Entities;
+
+namespace TechnicalTest.DDD.Infrastructure.Database
+{
+    public class PermissionEnitityStudentConfig : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.Property(p => p.Name).IsRequired();
+            builder.Property(p => p.LastName).IsRequired();
+            builder.Property(p => p.Career).IsRequired();
+            builder.Property(p => p.AdmissionDate).IsRequired();
+        }
+    }
+}
