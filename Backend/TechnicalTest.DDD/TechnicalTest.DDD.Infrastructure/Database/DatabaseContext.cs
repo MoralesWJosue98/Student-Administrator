07f113aef2a3a507bca3b52d172a67a94b07f1ba
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-21 17:29:59 -0600

    Add: configure student VO to known data types :card_file_box:

--- Backend/TechnicalTest.DDD/TechnicalTest.DDD.Infrastructure/Database/DatabaseContext.cs
+++ Backend/TechnicalTest.DDD/TechnicalTest.DDD.Infrastructure/Database/DatabaseContext.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using TechnicalTest.DDD.Domain.Entities;
+using TechnicalTest.DDD.Domain.ValueObjects;
 
 namespace TechnicalTest.DDD.Infrastructure.Database
 {
@@ -18,8 +19,42 @@ namespace TechnicalTest.DDD.Infrastructure.Database
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new PermissionEnitityStudentConfig());
+            modelBuilder.ConfigureStudent();
         }
-
-
     }
+    public static class ModelBuilderExtensions
+    {
+        public static void ConfigureStudent(this ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<Student>()
+                .Property(s => s.Name)
+                .HasColumnType("varchar")
+                .HasMaxLength(15)
+                .HasConversion(name => name.ToString(), nameString => StudentName.For(nameString));
+
+            modelBuilder
+                .Entity<Student>()
+                .Property(s => s.LastName)
+                .HasColumnType("varchar")
+                .HasMaxLength(15)
+                .HasConversion(lastName => lastName.ToString(), lastNameString => StudentLastName.For(lastNameString));
+
+            modelBuilder
+                .Entity<Student>()
+                .Property(s => s.Career)
+                .HasColumnType("varchar")
+                .HasMaxLength(15)
+                .HasConversion(career => career.ToString(), careerString => StudentCareer.For(careerString));
+
+            modelBuilder
+                .Entity<Student>()
+                .Property(s => s.AdmissionDate)
+                .HasColumnType("date")
+                .HasConversion(date => DateTime.Parse(date.ToString()), dateDate => StudentAdmissionDate.For(dateDate));
+
+
+        }
+    }
+
 }
