0493f05af7d01d6d812afdcce57ed824e4e79192
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-21 17:37:00 -0600

    Add: Database Migration :card_file_box:

---
+++ Backend/TechnicalTest.DDD/TechnicalTest.DDD.API/Migrations/20230221233116_Adding Student Table.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechnicalTest.DDD.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingStudentTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: false),
+                    LastName = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: false),
+                    Career = table.Column<string>(type: "varchar(15)", maxLength: 15, nullable: false),
+                    AdmissionDate = table.Column<DateTime>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
