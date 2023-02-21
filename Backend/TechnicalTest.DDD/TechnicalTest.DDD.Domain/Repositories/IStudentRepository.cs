7519ad7a3d69aa240d84abe43c7beab988bca502
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-21 17:50:20 -0600

    Add: Student repository in Domain Model for more abstraction :necktie:

--- Backend/TechnicalTest.DDD/TechnicalTest.DDD.Domain/Repositories/IStudentRepository.cs
+++ Backend/TechnicalTest.DDD/TechnicalTest.DDD.Domain/Repositories/IStudentRepository.cs
@@ -1,20 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using TechnicalTest.DDD.Domain.Entities;
 
 namespace TechnicalTest.DDD.Domain.Repositories
 {
-    public interface IStudentRepository<TEntity> where TEntity : class
+    public interface IStudentRepository : IRepository<Student>
     {
-        // Establishing the operations that will be needed
-        Task AddAsync(TEntity entity);
-        Task UpdateAsync(TEntity entity);   
-        Task DeleteAsync(TEntity entity);
-        Task<TEntity> GetByIdAsync(int id, IEnumerable<string> entitiesToInclude = null);
-        Task<IEnumerable<TEntity>> GetAllAsync(int skip, int limit, IEnumerable<Expression<Func<TEntity,bool>>> predicates = null, IEnumerable<string> entitiesToInclude = null);
-
     }
 }
