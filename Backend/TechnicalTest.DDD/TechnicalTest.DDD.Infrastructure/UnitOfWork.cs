ff6d0c347bd699fc64fc345541f2f5b96797ced8
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-22 12:27:45 -0600

    Add: unit of work implementation :necktie:

---
+++ Backend/TechnicalTest.DDD/TechnicalTest.DDD.Infrastructure/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicalTest.DDD.Domain;
+using TechnicalTest.DDD.Domain.Repositories;
+using TechnicalTest.DDD.Infrastructure.Database;
+
+namespace TechnicalTest.DDD.Infrastructure
+{
+    public class UnitOfWork : IUnitOfWork, IAsyncDisposable
+    {
+        private readonly DatabaseContext _databaseContext;
+
+        public IStudentRepository StudentRepository { get; private set; }   
+
+        public UnitOfWork(DatabaseContext databaseContext, IStudentRepository studentRepository)
+        {
+            _databaseContext = databaseContext;
+
+            StudentRepository= studentRepository;
+        }
+
+        public async Task<int> CompleteAsync()
+        {
+            return await _databaseContext.SaveChangesAsync();
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            return _databaseContext.DisposeAsync();
+        }
+    }
+}
