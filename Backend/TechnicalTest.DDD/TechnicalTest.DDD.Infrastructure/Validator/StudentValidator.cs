550cf800c3b8e659ee0ae17a4a8bb34fbd38b7f7
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-24 14:32:43 -0600

    Modify: added error message in student validator :necktie:

---
+++ Backend/TechnicalTest.DDD/TechnicalTest.DDD.Infrastructure/Validator/StudentValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicalTest.DDD.Domain.DTOs;
+
+namespace TechnicalTest.DDD.Infrastructure.Validator
+{
+    public class StudentValidator : AbstractValidator<StudentDto>
+    {
+        public StudentValidator() {
+            RuleFor(x => x.Id).NotEmpty().NotNull().WithMessage("Specify Student ID");
+            RuleFor(x => x.Name).NotEmpty().NotNull().WithMessage("Specify Student Name");
+            RuleFor(x => x.LastName).NotEmpty().NotNull().WithMessage("Specify Student Last Name");
+            RuleFor(x => x.Career).NotEmpty().WithMessage("Specify Student Career");
+            RuleFor(x => x.AdmissionDate).NotEmpty().WithMessage("Specify Student Admission Date");
+        }
+    }
+}
