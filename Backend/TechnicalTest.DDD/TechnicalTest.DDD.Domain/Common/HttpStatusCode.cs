73e3b8fff7a8aec4b05b06642981617a0d64f1b7
MoralesWJosue98 <wilfredo.josue@live.com>
2023-02-24 14:33:49 -0600

    Add: Http response validation :necktie:

---
+++ Backend/TechnicalTest.DDD/TechnicalTest.DDD.Domain/Common/HttpStatusCode.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechnicalTest.DDD.Domain.Common
+{
+    public enum HttpStatusCode
+    {
+        OK = 200,
+        Created = 201,
+        Accepted = 202,
+        BadRequest = 400,
+        Unauthorized = 401,
+        Forbidden = 403,
+        NotFound = 404,
+        MethodNotAllowed = 405,
+        Conflict = 409,
+        TooEarly = 425,
+        InternalServerError = 500,
+        BadGateway = 501,
+        ServiceUnavailable = 503,
+        HTTPVersionNotSupported = 505
+
+    }
+}
---
+++ Backend/TechnicalTest.DDD/TechnicalTest.DDD.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicalTest.DDD.Domain.Common;
+
+namespace TechnicalTest.DDD.Domain.Exceptions
+{
+    public class NotFoundException : BaseException
+    {
+        public NotFoundException() : base(HttpStatusCode.NotFound) { }
+
+        public NotFoundException(string message) : base(HttpStatusCode.NotFound, message)
+        {
+        }
+
+    }
+}
